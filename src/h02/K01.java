fc1a818941b894ceecec0aeff3b3a3b7752d66fd
kuudarashi <Creepas21@mail.com>
2020-09-11 14:51:35 +0200

    change "ik heb package h01 met K01 er bij gedaan"

---
+++ src/h02/K01.java
@@ -0,0 +1,18 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class K01 extends Applet {
+
+
+    public void init() {
+
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawString("hello hell0",50,50);
+    }
+}
+
