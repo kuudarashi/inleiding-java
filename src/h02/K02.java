a137d5796280261e1c3fbe5771797617cc9611b8
kuudarashi <Creepas21@mail.com>
2020-09-12 17:08:55 +0200

    change "heb h2 gemaakt"

---
+++ src/h02/K02.java
@@ -0,0 +1,18 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class K02 extends Applet {
+
+
+    public void init() {
+        setBackground(Color.BLUE);
+    }
+
+
+    public void paint(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.drawString("Robert Joel O'nyll Heyliger",50,60);
+    }
+}
