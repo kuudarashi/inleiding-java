002f585380b359e67ff2d02c64cb8a7faf40df50
kuudarashi <Creepas21@mail.com>
2020-09-13 13:31:56 +0200

    change "deel van h4"

--- src/h02/K04.java
+++ src/h02/K04.java
@@ -15,6 +15,9 @@ public class K04 extends Applet {
         g.drawLine(300,300,350,400);
         g.drawLine(300,300,250,400);
         g.drawLine(250,400,350,400);
+        g.drawLine(600,600,650,700);
+        g.drawLine(600,600,550,700);
+        g.drawRect(550,700,100,100);
 
     }
 }
