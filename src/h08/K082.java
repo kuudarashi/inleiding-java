30ac41ab1de65f26af2b42f0f8ab7ab2df80be4a
Onyll <creepas21@gmail.com>
2020-09-29 15:54:20 +0200

    change "8.3 af"

--- src/h08/K082.java
+++ src/h08/K082.java
@@ -1,16 +1,46 @@
 package h08;
 
+import javax.xml.transform.OutputKeys;
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
 
 public class K082 extends Applet {
-    int e;
-    Label d;
-    Button w;
-    TextField s;
+    double a1,a2;
+    Label lab;
+    Button ok;
+    TextField txt;
 
     public void init() {
+        ok = new Button("OK");
+        lab = new Label("prijs:");
+        txt = new TextField(20);
+        ok.addActionListener( new K082.KO());
+        txt.addActionListener( new K082.XTX());
+        add(ok);
+        add(lab);
+        add(txt);
 
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("prijs:" + a1,50,50);
+
+    }
+    class KO implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+
+            a1= a1/100*121;
+            repaint();
+        }
+    }
+
+    class XTX implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            a1= Double.parseDouble(txt.getText());
+            repaint();
+        }
     }
 }
