191e50c32ff3d5c00141d25bd7dcc7c276cfefa6
Onyll <creepas21@gmail.com>
2020-09-30 14:05:29 +0200

    change "h8 af"

--- src/h08/KP8.java
+++ src/h08/KP8.java
@@ -2,20 +2,32 @@ package h08;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class KP8 extends Applet {
-    int a1;
+    double a1,a2,uitkomst;
     TextField txt,txt2;
-    Button p,m,x,d;
+    Button p;
+    Button m;
+    Button x;
+    Button d;
 
 
     public void init() {
+        setBackground(Color.DARK_GRAY);
         p= new Button("+");
         m= new Button("-");
         x= new Button("X");
         d= new Button("/");
+        p.addActionListener(new plus());
+        m.addActionListener(new minus());
+        x.addActionListener(new times());
+        d.addActionListener(new divided());
         txt= new TextField(20);
         txt2= new TextField(20);
+        txt.addActionListener(new nmbrs());
+        txt2.addActionListener(new nmbrs());
         add(p);
         add(m);
         add(x);
@@ -27,6 +39,42 @@ public class KP8 extends Applet {
 
 
     public void paint(Graphics g) {
+        g.drawString("uitkomst:" + uitkomst,60,80);
+    }
 
+    class plus implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+         uitkomst= a1 + a2;
+         repaint();
+        }
+    }
+
+    class minus implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            uitkomst= a1 - a2;
+            repaint();
+        }
+    }
+
+    class times implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            uitkomst= a1 * a2;
+            repaint();
+        }
+    }
+
+    class divided implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            uitkomst= a1 / a2;
+            repaint();
+        }
+    }
+
+    class nmbrs implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            a1= Double.parseDouble(txt.getText());
+            a2= Double.parseDouble(txt2.getText());
+            repaint();
+        }
     }
 }
