ce2d1994c63afa378b28b925aad1062981b2bf3a
Onyll <creepas21@gmail.com>
2020-10-21 13:33:43 +0200

    change "h10 af (ben echt lanzaam XD)"

--- src/h10/K104.java
+++ src/h10/K104.java
@@ -1,32 +1,33 @@
 package h10;
-// dont forget to change
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class K104 extends Applet {
-    double a, b, c,totaal, gemiddeld;
+    double a,totaal, gemiddeld;
     int cijfers;
     TextField txt;
     Button butt;
-    String tekst,g;
+    String tekst,txt2,gm,g;
 
     public void init() {
         butt = new Button("ok");
         add(butt);
+        butt.addActionListener(new on ());
         txt = new TextField("", 5);
         add(txt);
         txt.addActionListener(new txv());
         tekst = "";
         totaal = 0.0;
         cijfers = 0;
-        gemiddeld= 0.0;
+        txt2 ="";
     }
 
     public void paint(Graphics g) {
         g.drawString(tekst,50,50);
-        g.drawString("het gemiddelde is: " + gemiddeld,50,70);
+        g.drawString(gm + gemiddeld,50,70);
+        g.drawString(txt2,50,90);
     }
     class txv implements ActionListener{
         public void actionPerformed(ActionEvent e) {
@@ -35,7 +36,7 @@ public class K104 extends Applet {
          a = Double.parseDouble(g);
          totaal += a;
          cijfers ++;
-         gemiddeld = totaal / cijfers;
+
 
          if (a >= 5.5) {
              tekst = "voldoende";
@@ -46,5 +47,18 @@ public class K104 extends Applet {
          repaint();
         }
     }
+    class on implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            gemiddeld = totaal / cijfers;
+            gm = "gemiddelde: ";
+            if (gemiddeld >= 5.5) {
+                txt2 = "gefeliciteerd u bent geslaagd!";
+            } else {
+                txt2 = "helaas u bent gezakt";
+            }
+            repaint();
+        }
+    }
 }
+// dont forget to make null 0.0 disappear
 
---
+++ src/h10/KP10.java
@@ -0,0 +1,66 @@
+package h10;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class KP10 extends Applet {
+    int a;
+    TextField txv;
+    String txt;
+
+    public void init() {
+        txv = new TextField("",5);
+        txv.addActionListener(new txtfield ());
+        add(txv);
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(txt,50,50);
+    }
+
+    class txtfield implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            txt = txv.getText();
+            a = Integer.parseInt(txt);
+            switch (a) {
+                case 1:
+                    txt = "slecht";
+                    break;
+                case 2:
+                    txt = "slecht";
+                    break;
+                case 3:
+                    txt = "slecht";
+                    break;
+                case 4:
+                    txt = "onvoldoende";
+                    break;
+                case 5:
+                    txt = "matig";
+                    break;
+                case 6:
+                    txt = "voldoende";
+                    break;
+                case 7:
+                    txt = "voldoende";
+                    break;
+                case 8:
+                    txt = "goed";
+                    break;
+                case 9:
+                    txt = "goed";
+                    break;
+                case 10:
+                    txt = "goed";
+                    break;
+                default:
+                    txt= "geen geldig getal!";
+
+            }
+            repaint();
+        }
+    }
+}
